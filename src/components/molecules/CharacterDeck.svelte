495fe2d342ff77deec71548e6db4d1611f21daa2
Jake Chorley <jakechorley@jakes-mbp-2.home>
2019-10-26 10:43:18 +0100

    After building first component

--- src/components/molecules/CharacterDeck.svelte
+++ src/components/molecules/CharacterDeck.svelte
@@ -1,9 +1,5 @@
 <script>
-  import Character from "../atoms/Character.svelte";
 
-  export let characters = ["", ""];
-  export let handleCharacterClick;
-  export let player;
 </script>
 
 <style>
@@ -21,15 +17,3 @@
     margin-bottom: 5px;
   }
 </style>
-
-<div>
-  <div />
-  {#each characters as { name, src }, i (name)}
-    <article
-      on:click={handleCharacterClick}
-      data-index={i}
-      data-player={player}>
-      <Character {name} {src} />
-    </article>
-  {/each}
-</div>
